0dba19293d23bc7de29a30e8630dfce4177d6b93
Sandun Tharuka <sandun.suntws@gmail.com>
2022-10-02 00:43:13 +0530

    set input value dynamicaly

--- components/PurchTable.js
+++ components/PurchTable.js
@@ -5,114 +5,177 @@ import Constants from 'expo-constants';
 import Checkbox from 'expo-checkbox';
 import useAuth from '../hooks/useAuth';
 import axios from 'axios';
-const PurchTable = ({setMainScanerDisplay,id,textSub,setTextSub,scanned,setScanned}) => {
+import SelectList from 'react-native-dropdown-select-list'
+import { getBincode, getBincodeWithQty, getSupplyQty } from '../function/func';
+const PurchTable = ({ setMainScanerDisplay, id, textSub, setTextSub, scanned, setScanned }) => {
 
-    const { po, setPo,poInitial,setPoInitial,setPoLine,poLine } = useAuth()
-    const [isChecked, setChecked] = useState(false);
-    const [isMultipeChecked, setMultipeChecked] = useState(false);
-    const [subPoLine, setSubPoLines] = useState([]);
-    
-    
-      const [gender, setGender] = useState([
+  const { po, setPo, poInitial, setPoInitial, setPoLine, poLine } = useAuth()
+  const [isChecked, setChecked] = useState(false);
+  const [isMultipeChecked, setMultipeChecked] = useState(false);
+  const [subPoLine, setSubPoLines] = useState([]);
+  const [selected, setSelected] = useState("");
+  const [text, setText] = useState("");
+
+  const data = [{ key: '1', value: 'Jammu ' }, { key: '2', value: 'san ' }, { key: '3', value: 'Jammlllllllljjjjjjjjjjjjjkku ' }];
+
+  const [gender, setGender] = useState([
     { label: "Male", value: "male" },
     { label: "Female", value: "female" },
     { label: "Prefer Not to Say", value: "neutral" },
   ]);
-    
-               
-const handleFilter =()=>{
-setPo(po=>po.filter(item => item.seqNo ===id))
-}
-const handleRemove =()=>{
-  setPo(()=>poInitial)
+
+
+  const handleFilter = () => {
+    setPo(po => po.filter(item => item.seqNo === id))
+  }
+  const handleRemove = () => {
+    setPo(() => poInitial)
   }
 
 
 
 
   const getPurchaseOrdersLine = async () => {
-;
-   
+    ;
+
     try {
       const res = await axios.get(`http://192.168.1.200:3000/purchaseorderline/purchaseorder/${id}`, { headers: { 'content-Type': 'application/json' } })
-
-      setPoLine(res.data)
+      const data = res.data.map((element) => {
+        return {
+          binCodeArrayWithQty: getBincodeWithQty(element),
+          supplyQty: getSupplyQty({ ...element, binCodeArrayWithQty: getBincodeWithQty(element) }),
+          ...element,
+        }
+      })
+      setPoLine(data)
       console.log(res.data);
-     
+
     } catch (error) {
       console.log(error);
     }
-    
+
   }
 
 
 
   useEffect(() => {
-   
-    
-      getPurchaseOrdersLine()
-      console.log('use effect 3');
-    
+
+
+    getPurchaseOrdersLine()
+    console.log('use effect 3');
+
 
   }, [])
 
   useEffect(() => {
-   
+
     if (textSub.length > 3) {
-      const a= poLine.find(item => item.stockCode ===textSub )
+      const a = poLine.find(item => item.stockCode === textSub)
       console.log(a);
-      if (a && !subPoLine.find( ({ stockCode }) => stockCode === a?.stockCode )) {
-        setSubPoLines(pre=>[...pre,a])
+      if (a && !subPoLine.find(({ stockCode }) => stockCode === a?.stockCode)) {
+        setSubPoLines(pre => [...pre, { ...a, binCodeArray: getBincode(a), }])
       }
-      
+
     }
-  
-
-}, [textSub])
 
 
-console.log(poLine);
+  }, [textSub])
 
 
+  console.log(poLine);
 
 
 
-const onChangeValue =(item,index)=>{
-  const newData =subPoLine.map(newItem=>{
-    if(newItem.stockCode ==item.stockCode){
+
+
+  const onChangeValue = (item, index) => {
+    const newData = subPoLine.map(newItem => {
+      if (newItem.stockCode == item.stockCode) {
+        return {
+          ...newItem, selected: !item.selected
+        }
+      }
       return {
-        ...newItem,selected:!item.selected
+        ...newItem
       }
-    }
-    return{
-      ...newItem
-    }
-  })
-  setSubPoLines(newData)
-}
+    })
+    setSubPoLines(newData)
+  }
 
 
-const onChangeValue2 =(item,index)=>{
-  const newData =subPoLine.map(newItem=>{
-    if(newItem.stockCode ==item.stockCode){
+  const onChangeValue2 = (item, index) => {
+    const newData = subPoLine.map(newItem => {
+      if (newItem.stockCode == item.stockCode) {
+        return {
+          ...newItem, mselected: !item.mselected
+        }
+      }
       return {
-        ...newItem,mselected:!item.mselected
+        ...newItem
       }
-    }
-    return{
-      ...newItem
-    }
-  })
-  setSubPoLines(newData)
-}
+    })
+    setSubPoLines(newData)
+  }
+
+  const onChangeValue3 = (item, index) => {
+    const newData = subPoLine.map(newItem => {
+      if (newItem.stockCode == item.stockCode) {
+        console.log(newItem.binCodeArray);
+        return {
+          ...newItem, binCode: selected
+        }
+      }
+      return {
+        ...newItem
+      }
+    })
+    setSubPoLines(newData)
+  }
+
+  const onChangeValue4 = (item, e) => {
+
+    const newData = subPoLine.map(newItem => {
+      if (newItem.stockCode == item.stockCode) {
+
+        return {
+          ...newItem, supplyQty: e
+        }
+      }
+      return {
+        ...newItem
+      }
+    })
+    setSubPoLines(newData)
+  }
+
+
+  const onChangeValue5 = (item, d, e) => {
+
+    const newData = subPoLine.map(newItem => {
+      if (newItem.stockCode == item.stockCode) {
+        const newBinArray = newItem.binCodeArrayWithQty.map(ele => {
+          if (ele.binCode == d.binCode) {
+            return { ...ele, qty: e }
+          }
+          return ele
+        })
+
+
+        return {
+          ...newItem, binCodeArrayWithQty: newBinArray, supplyQty: getSupplyQty({ ...newItem, binCodeArrayWithQty: newBinArray })
+        }
+      }
+      return {
+        ...newItem
+      }
+    })
+    setSubPoLines(newData)
+  }
 
 
 
 
-
-
-
-
+  console.log(subPoLine);
 
 
 
@@ -122,33 +185,33 @@ const onChangeValue2 =(item,index)=>{
 
   return (
     <View>
-         <View  style={{
+      <View style={{
         flexDirection: 'column',
         justifyContent: 'flex-start',
-        alignItems:'center',
-        
-        
+        alignItems: 'center',
+
+
       }} >
-        </View>
-       <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center',marginBottom:5}}>
-      { po?.length === 1 &&<TextInput placeholder='Stockcode' style={{ 
-    	height: 40, 
-      width:120,
-    	borderColor: 'gray', 
-    	borderWidth: 1,
-      padding: 10,
-     textAlign:'auto',
-      marginTop:10,
-      borderRadius:5,
-      marginLeft:10
-    }} onChangeText={(e) => setTextSub(e)} value={textSub}  /> }
-{po?.length === 1 &&<TouchableOpacity style={styles.buttonTwo} onPress={() => {setScanned(false), setTextSub('')}}>
-      <Text style={styles.text}>Scan sub</Text>
-    </TouchableOpacity>}
+      </View>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 5 }}>
+        {po?.length === 1 && <TextInput placeholder='Stockcode' style={{
+          height: 40,
+          width: 120,
+          borderColor: 'gray',
+          borderWidth: 1,
+          padding: 10,
+          textAlign: 'auto',
+          marginTop: 10,
+          borderRadius: 5,
+          marginLeft: 10
+        }} onChangeText={(e) => setTextSub(e)} value={textSub} />}
+        {po?.length === 1 && <TouchableOpacity style={styles.buttonTwo} onPress={() => { setScanned(false), setTextSub('') }}>
+          <Text style={styles.text}>Scan sub</Text>
+        </TouchableOpacity>}
 
-   
 
-{/*       
+
+        {/*       
      { po?.length > 1 && <TouchableOpacity style={{backgroundColor:'orange',width:70,padding:10,borderRadius:5,
       marginTop:10,justifyContent:'center',alignItems:'center',marginRight:5}} onPress={handleFilter}>
       <Text style={styles.text}>Pick</Text>
@@ -158,52 +221,74 @@ const onChangeValue2 =(item,index)=>{
       marginTop:10,justifyContent:'center',alignItems:'center',marginRight:5}} onPress={handleRemove}>
       <Text style={styles.text}>UnPick</Text>
     </TouchableOpacity>} */}
-        
-     
-    
-       </View>
-       <View >
-       {subPoLine.length>0 && <View style={{flexDirection:'row',justifyContent:'space-around',padding:14,backgroundColor:'blue',margin:5}}>
-        <Text style={styles.textg}>STOCKCODE</Text>
-      <Text style={styles.textg}>ORDER QTY</Text>
-      <Text style={styles.textg}>BINCODE</Text>
-      <Text style={styles.textg}>M-SELECT</Text>
-      <Text style={styles.textg}>PICKNOW</Text>
-      <Text style={styles.textg}>SELECT</Text>
-     
+
+
+
+      </View>
+      <View >
+        {subPoLine.length > 0 && <View style={{ flexDirection: 'row', justifyContent: 'space-around', padding: 14, backgroundColor: 'blue', margin: 5 }}>
+          <Text style={styles.textg}>STOCKCODE</Text>
+          <Text style={styles.textg}>ORDER QTY</Text>
+          <Text style={styles.textg}>BINCODE</Text>
+          <Text style={styles.textg}>M-SELECT</Text>
+          <Text style={styles.textg}>PICKNOW</Text>
+          <Text style={styles.textg}>SELECT</Text>
+
         </View>}
-      { subPoLine.length>0 && subPoLine.map((item,i)=><View style={{flexDirection:'row',justifyContent:'space-around',backgroundColor:'blue',margin:5,padding:14,borderRadius:10,direction:'row',alignItems:'center'}} key={i}> 
-      <Text style={styles.textg}>{item.stockCode}</Text>
-      <Text style={styles.textg}>{item.orderQty
-      }</Text>
-        <View style={styles.drop}>
-      
-      <DropDownPicker
-          items={[
-              {label: 'English', value: 'en'},
-              {label: 'Deutsch', value: 'de'},
-              {label: 'French', value: 'fr'},
-          ]}
-          defaultIndex={0}
-          containerStyle={{height: 40}}
-          onChangeItem={item => console.log(item.label, item.value)}
-      />
-    </View>
-      <Checkbox
-          style={styles.checkbox}
-          value={item?.mselected}
-          onValueChange={()=>onChangeValue2(item,i)}
-          color={item.mselected ? '#4630EB' : undefined}
-        />
-      <Text style={styles.textg}>{item.supplyQty}</Text>
-      <Checkbox
-          style={styles.checkbox}
-          value={item?.selected}
-          onValueChange={()=>onChangeValue(item,i)}
-          color={item.selected ? '#4630EB' : undefined}
-        />
-     
-      </View>)               }
+        {subPoLine.length > 0 && subPoLine.map((item, i) => <View style={{ flexDirection: 'row', justifyContent: 'space-around', backgroundColor: 'blue', margin: 5, padding: 20, borderRadius: 10, direction: 'row', alignItems: 'center' }} key={i}>
+          <Text style={styles.textg}>{item.stockCode}</Text>
+          <Text style={styles.textg}>{item.orderQty
+          }</Text>
+
+
+          {item?.mselected ? <View>{item.binCodeArrayWithQty.map((d, i) => <View key={i} style={{flexDirection: 'row', justifyContent: 'space-around', padding: 14}}>
+            <Text>{d.binCode}</Text>
+
+            <TextInput style={{
+              height: 30,
+              width: 60,
+              borderColor: 'gray',
+              borderWidth: 1,
+              padding: 1,
+              textAlign: 'center',
+              marginTop: 1,
+              borderRadius: 5,
+              marginLeft: 1
+            }} onChangeText={(e) => onChangeValue5(item, d, e)} value={`${d.qty}`} keyboardType='numeric' />
+
+          </View>)}
+          </View> : <SelectList onSelect={() => onChangeValue3(item, i)}
+            setSelected={setSelected}
+            data={item?.binCodeArray} search={false}
+            boxStyles={{ height: 50, borderRadius: 10, width: 100, backgroundColor: 'blue', padding: 0 }} //override default styles
+            defaultOption={{ key: '1', value: item?.binCodeArray[0] }}
+          />}
+
+          <Checkbox
+            style={styles.checkbox}
+            value={item?.mselected}
+            onValueChange={() => onChangeValue2(item, i)}
+            color={item.mselected ? '#4630EB' : undefined}
+          />
+          <TextInput style={{
+            height: 30,
+            width: 60,
+            borderColor: 'gray',
+            borderWidth: 1,
+            padding: 1,
+            textAlign: 'center',
+            marginTop: 1,
+            borderRadius: 5,
+            marginLeft: 1
+          }} onChangeText={(e) => onChangeValue4(item, e)} value={`${item.supplyQty}`} keyboardType='numeric' />
+          <Checkbox
+            style={styles.checkbox}
+            value={item?.selected}
+            onValueChange={() => onChangeValue(item, i)}
+            color={item.selected ? '#4630EB' : undefined}
+          />
+
+        </View>)}
       </View>
     </View>
   )
@@ -214,92 +299,92 @@ export default PurchTable
 
 
 const styles = StyleSheet.create({
-    buttonTwo: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 10,
-      paddingHorizontal: 2,
-      borderRadius: 5,
-      elevation: 3,
-      backgroundColor: 'blue',
-      width:100,
-      marginTop:10,
-      marginRight:10
-      
-    },
-    textg: {
-      fontSize: 10,
-      lineHeight: 21,
-      fontWeight: 'bold',
-      letterSpacing: 0.25,
-      color: 'white',
-    },
-    barcodebox:{
-        alignItems: 'center',
-        justifyContent: 'center',
-        height:150,
-        width:370,
-        overflow:'hidden',
-        borderRadius:30,
-        backgroundColor:'red',
-        marginTop:5
-    },
-    container:{
-      paddingTop:4,
-      backgroundColor:'#ddd', 
+  buttonTwo: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 2,
+    borderRadius: 5,
+    elevation: 3,
+    backgroundColor: 'blue',
+    width: 100,
+    marginTop: 10,
+    marginRight: 10
 
-    },button: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 12,
-      paddingHorizontal: 32,
-      borderRadius: 5,
-      elevation: 3,
-      backgroundColor: 'blue',
-      width:180,
-      marginTop:10,
-      marginRight:10
-    },
-    text: {
-      fontSize: 16,
-      lineHeight: 21,
-      fontWeight: 'bold',
-      letterSpacing: 0.25,
-      color: 'white',
-    },
-    barcodebox:{
-        alignItems: 'center',
-        justifyContent: 'center',
-        height:150,
-        width:370,
-        overflow:'hidden',
-        borderRadius:30,
-        backgroundColor:'black',
-        marginTop:5
-    },
-    container:{
-      paddingTop:4,
-      backgroundColor:'#ddd', 
+  },
+  textg: {
+    fontSize: 10,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  barcodebox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 150,
+    width: 370,
+    overflow: 'hidden',
+    borderRadius: 30,
+    backgroundColor: 'red',
+    marginTop: 5
+  },
+  container: {
+    paddingTop: 4,
+    backgroundColor: '#ddd',
 
-    },
-    
-    section: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      marginRight:15
-    },
-    paragraph: {
-      fontSize: 15,
-    },
-    checkbox: {
-      margin: 8,
-    },
-    drop: {
-      flex: 1,
-      justifyContent: 'center',
-      paddingTop: Constants.statusBarHeight,
-      backgroundColor: '#ecf0f1',
-      padding: 8,
-    },
-   
-  });
+  }, button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 5,
+    elevation: 3,
+    backgroundColor: 'blue',
+    width: 180,
+    marginTop: 10,
+    marginRight: 10
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+  barcodebox: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 150,
+    width: 370,
+    overflow: 'hidden',
+    borderRadius: 30,
+    backgroundColor: 'black',
+    marginTop: 5
+  },
+  container: {
+    paddingTop: 4,
+    backgroundColor: '#ddd',
+
+  },
+
+  section: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 15
+  },
+  paragraph: {
+    fontSize: 15,
+  },
+  checkbox: {
+    margin: 8,
+  },
+  drop: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+
+});
--- function/func.js
+++ function/func.js
@@ -3,3 +3,44 @@ export const getDate =(date)=>{
     return date?.split(' ')[0]
 
 }
+
+export  const  getBincode =(data) =>{
+    const code = []
+
+    data?.stockLocationBinInfo.length > 0 &&
+      data?.stockLocationBinInfo.forEach((element) => {
+        if (element.binCode && element.location === data.location) {
+          code.push(element.binCode)
+        }
+      })
+
+    return code
+  }
+
+  export const getBincodeWithQty =(data)=> {
+    const code = []
+
+    data?.stockLocationBinInfo.length > 0 &&
+      data?.stockLocationBinInfo.forEach((element) => {
+        if (element.binCode && element.location === data.location) {
+          code.push({
+            binCode: element.binCode,
+            qty: Number(element.qty),
+            currentQty: Number(element.qty),
+          })
+        }
+      })
+
+    return code
+  }
+
+
+  export const getSupplyQty=(item) =>{
+    return item.binCodeArrayWithQty.reduce(function (
+      accumulator,
+      currentValue
+    ) {
+      return accumulator + Number(currentValue.qty)
+    },
+    0)
+  }
